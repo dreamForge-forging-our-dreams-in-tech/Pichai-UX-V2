c764244312490c86a86467b281135189f878af8c
Luke-technolab <luke@technolableiden.nl>
2026-03-25 10:28:04 +0100

    simplified shortcode_registery

--- shortcode_registery.php
+++ shortcode_registery.php
@@ -1,52 +1,39 @@
 <?php
-//registreert wordpress shortcodes met variables
-function github_repos_shortcode($atts)
-{
-    // 1. Define defaults and merge with user input
-    $a = shortcode_atts(array(
-        'orgName' => 'dreamForge-forging-our-dreams-in-tech', // default value
-    ), $atts);
-
-    // 2. Return the HTML output
-    return '<github-repositories orgName="' . $a['orgName'] . '" class="grid-container"></github-repositories>';
+function wc_tag(string $tag, array $attrs = [], string $class = 'grid-container'): string {
+    $html = "<$tag" . ($class ? " class=\"$class\"" : '');
+    foreach ($attrs as $key => $val) {
+        $html .= ' ' . $key . '="' . esc_attr($val) . '"';
+    }
+    return "$html></$tag>";
 }
-add_shortcode('github-repos', 'github_repos_shortcode');
 
-add_shortcode('github-issues', function ($atts) {
-    // 1. Change 'repoUrl' to 'repourl' to match what the browser actually sees
-    $a = shortcode_atts(array(
+function sc_github_repos($atts) {
+    return wc_tag('github-repositories', shortcode_atts([
+        'orgName' => 'dreamForge-forging-our-dreams-in-tech',
+    ], $atts ?? []), 'grid-container');
+}
+add_shortcode('github-repos', 'sc_github_repos');
+
+function sc_github_issues($atts) {
+    return wc_tag('github-issues', shortcode_atts([
         'repourl' => 'https://api.github.com/repos/dreamForge-forging-our-dreams-in-tech/The-Magic-Garden/issues?state=all&per_page=100',
-    ), $atts);
-
-    // 2. Render it with the lowercase attribute name
-    return '<github-issues repourl="' . esc_attr($a['repourl']) . '" class="grid-container github-issues"></github-issues>';
-});
-
-add_shortcode('github-login', function () {
-    return '<github-login></github-login>';
-});
-
-function store_tile_shortcode($atts)
-{
-    // 1. Keys must be LOWERCASE here to match WordPress processing
-    $a = shortcode_atts(array(
-        'name' => 'default-store',
-        'price' => '0.00',
-        'release' => '2024-01-01',
-        'shortdescription' => 'No description available.', // Lowercase
-        'storeurl' => '#',                          // Lowercase
-        'imagesrc' => 'https://via.placeholder.com/150', // Lowercase
-    ), $atts);
-
-    // 2. Escape the output for security and stability
-    return sprintf(
-        '<store-tile name="%s" price="%s" release="%s" shortDescription="%s" storeUrl="%s" imageSrc="%s" class="grid-container"></store-tile>',
-        esc_attr($a['name']),
-        esc_attr($a['price']),
-        esc_attr($a['release']),
-        esc_attr($a['shortdescription']),
-        esc_attr($a['storeurl']),
-        esc_attr($a['imagesrc'])
-    );
+    ], $atts ?? []), 'grid-container github-issues');
 }
-add_shortcode('store-tile', 'store_tile_shortcode');
+add_shortcode('github-issues', 'sc_github_issues');
+
+function sc_github_login($atts) {
+    return wc_tag('github-login', [], '');
+}
+add_shortcode('github-login', 'sc_github_login');
+
+function sc_store_tile($atts) {
+    return wc_tag('store-tile', shortcode_atts([
+        'name'             => 'default-store',
+        'price'            => '0.00',
+        'release'          => '2024-01-01',
+        'shortdescription' => 'No description available.',
+        'storeurl'         => '#',
+        'imagesrc'         => 'https://via.placeholder.com/150',
+    ], $atts ?? []), 'grid-container');
+}
+add_shortcode('store-tile', 'sc_store_tile');
